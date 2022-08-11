fc8cb578cc3fb34743fb9c69000e20e75db3bc06
Sudarshan <sudarshaana@gmail.com>
2022-08-11 21:49:14 +0600

    venv added, model added, image preview added to the admin panel

---
+++ pdf/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import PDF
+from django.utils.html import format_html
+
+
+class PDFAdmin(admin.ModelAdmin):
+
+    def thumb_f(self, obj):
+        return format_html('<img src="{}" width="auto" height="150px" /> '.format(obj.thumb.url))
+
+    thumb_f.short_description = 'thumb'
+
+    list_display = ['name', 'thumb_f', 'pdf']
+
+
+admin.site.register(PDF, PDFAdmin)
