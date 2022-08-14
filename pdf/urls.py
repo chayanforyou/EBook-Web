98bce7304012d7468ab9159a22f385c558858390
Chayan <chayanforyou007@gmail.com>
2022-08-14 09:23:08 +0600

    clean repo

--- pdf/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls.conf import path
-from .views import pdf_list
-
-urlpatterns = [
-    path('', pdf_list, name="pdf_list")
-]
--- pdf/views.py
+++
@@ -1,8 +0,0 @@
-from .models import PDF
-from django.http import JsonResponse
-
-
-def pdf_list(request):
-    objects = PDF.objects.all().values("id", "name", "thumb", "pdf").order_by('-id')
-    data = list(objects)
-    return JsonResponse(data, safe=False)
